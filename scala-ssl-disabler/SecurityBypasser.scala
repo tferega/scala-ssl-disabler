7ce804a1d925eaac32b17cfd703a451ea825de47
Tomo Ferega <tferega@gmail.com>
2017-01-22 19:08:34 +0100

    Minor style fixes

--- scala-ssl-disabler/SecurityBypasser.scala
+++ scala-ssl-disabler/SecurityBypasser.scala
@@ -6,26 +6,23 @@ object SecurityBypasser {
   import java.security.cert.X509Certificate
   import javax.net.ssl._
 
-
   // The all-permisive trust manager.
   object AllTM extends X509TrustManager {
-    def getAcceptedIssuers(): Array[X509Certificate] = null
+    def getAcceptedIssuers: Array[X509Certificate] = null
     def checkClientTrusted(certs: Array[X509Certificate], authType: String) {}
     def checkServerTrusted(certs: Array[X509Certificate], authType: String) {}
   }
-
 
   // The all-permissive hostname verifier.
   object AllHosts extends HostnameVerifier {
     def verify(urlHostName: String, session: SSLSession) = true
   }
 
-
   def destroyAllSSLSecurityForTheEntireVMForever() {
     val trustAllCerts = Array[TrustManager](AllTM)
     val sslContext = SSLContext.getInstance("SSL")
-    sslContext.init(null, trustAllCerts, null);
-    HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory())
-    HttpsURLConnection.setDefaultHostnameVerifier(AllHosts);
+    sslContext.init(null, trustAllCerts, null)
+    HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory)
+    HttpsURLConnection.setDefaultHostnameVerifier(AllHosts)
   }
 }
